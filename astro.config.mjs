4e9ac525e44444d7fc8bc8274093840e8f2e28dc
Chris Chinchilla <chris@chrischinchilla.com>
2022-12-05 16:01:26 +0100

    Tidy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,1 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [svelte()]
-});
