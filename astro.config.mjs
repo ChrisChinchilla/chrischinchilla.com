6bf78b7d7acd971d3e42a5e88c291b234ec11c02
Chris Chinchilla <chris@chrischinchilla.com>
2022-12-18 11:39:29 +1100

    Create aggregation pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 import { injectFrontmatter } from "./src/utils/remark";
+import react from '@astrojs/react';
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     extendDefaultPlugins: true,
-    remarkPlugins: [injectFrontmatter],
-
+    remarkPlugins: [injectFrontmatter]
   },
-
+  integrations: [react()],
+  output: "server",
+  adapter: netlify()
 });
