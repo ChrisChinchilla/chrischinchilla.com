66e1642a0116b57ac05b9bbe6aa0b1e49eb1417a
Chris Chinchilla <chris@chrischinchilla.com>
2025-06-23 18:43:15 +0200

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,17 +10,13 @@ import { remarkReadingTime } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/config.mjs';
 // import react from "@astrojs/react";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const whenExternalScripts = (items = []) => SITE.googleAnalyticsId ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+const whenExternalScripts = (items = []) =>
+  SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    responsiveImages: true,
-  },
   image: {
-    // Used for all Markdown images; not configurable per-image
-    // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: 'constrained',
+    responsiveStyles: true,
   },
   site: SITE.origin,
   base: SITE.basePathname,
@@ -29,34 +25,58 @@ export default defineConfig({
   integrations: [
     icon({
       include: {
-        tabler: ['brand-youtube', 'brand-medium', 'brand-patreon', 'brand-github', 'brand-linkedin', 'mail', 'brand-bluesky', 'brand-tiktok', 'brand-twitter', 'brand-threads', 'brand-mastodon','menu','chevron-down','rss','brand-discord','download','brand-facebook'],
+        tabler: [
+          'brand-youtube',
+          'brand-medium',
+          'brand-patreon',
+          'brand-github',
+          'brand-linkedin',
+          'mail',
+          'brand-bluesky',
+          'brand-tiktok',
+          'brand-twitter',
+          'brand-threads',
+          'brand-mastodon',
+          'menu',
+          'chevron-down',
+          'rss',
+          'brand-discord',
+          'download',
+          'brand-facebook',
+        ],
         'simple-line-icons': ['social-spotify'],
         fa: ['podcast'],
         carbon: ['calendar-heat-map'],
         fluent: ['people-audience-20-filled'],
         ri: ['money-euro-circle-fill'],
         ph: ['pencil-circle-bold'],
-        'simple-icons': ['applepodcasts', 'amazonmusic','pocketcasts']
-      }
+        'simple-icons': ['applepodcasts', 'amazonmusic', 'pocketcasts'],
+      },
     }),
     tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap(), mdx(), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  }))], 
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap(),
+    mdx(),
+    ...whenExternalScripts(() =>
+      partytown({
+        config: {
+          forward: ['dataLayer.push'],
+        },
+      })
+    ),
+  ],
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true
+    extendDefaultPlugins: true,
   },
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src')
-      }
-    }
-  }
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
